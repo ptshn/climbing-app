3c84ab4e858a279bb7575a0fe8f4445c16b927e9
Paul Shin <Paul@Pauls-MacBook-Air.local>
2020-07-16 11:02:05 -0700

    add total count and distance for bolt holes

--- src/wall/Grid.js
+++ src/wall/Grid.js
@@ -16,37 +16,57 @@ const Panel = styled.div`
 
 const GridContainer = styled.div`
     display: grid;
-    grid-template-rows: 18px 33px 33px 33px 33px 33px 33px 33px 33px 33px 33px 33px 18px;
-    grid-template-columns: 33px 67px 67px 67px 67px 67px 67px 67px 67px 67px 66px 66px 33px;
+    grid-template-rows: 18px 33px 33px 33px 33px 33px 33px 33px 33px 33px 33px 33px 17px;
+    grid-template-columns: 33px 67px 67px 67px 67px 67px 67px 67px 67px 67px 66px 66px 34px;
 `;
 
 const ContentDiv = styled.div`
-    border: 1px solid;
     display: flex;
     justify-content: center;
     align-items: center;
+    position: relative;
+
+    &:nth-child(even) {
+        visibility: hidden;
+    }
+`;
+
+const Dot = styled.span`
+    background: black;
+    border-radius: 50%;
+    width: 5px;
+    height: 5px;
+    position: absolute;
+    bottom: 0;
+    right: 0;
 `;
 
 const Grid = props => {
     const cellTotal = [];
+    const excludeNumberList = [13, 39, 65, 91, 117, 143, 157, 159, 161, 163, 165, 167, 169];
 
     for (let i = 1; i <= 169; i++) {
         cellTotal.push(i);
     }
 
-    const cell = cellTotal.map(num => {
+
+    const boltHole = cellTotal.map(num => {
         return (
             <ContentDiv key={num}>
-                {num}
+                {excludeNumberList.indexOf(num) === -1 ? 
+                    <Dot></Dot>
+                    :
+                    null
+                }
             </ContentDiv>
         )
     })
-
+    
     return (
         <Frame>
             <Panel>
                 <GridContainer>
-                    {cell}
+                    {boltHole}
                 </GridContainer>
             </Panel>
         </Frame>
